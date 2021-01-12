3e2f3277c3f928ecd521691fd03d71a7b50bf66f
billettc <charles@cbillette.com>
2021-01-12 09:15:20 -0500

    added fast-delete: multi threaded gcloud objects delete.

---
+++ cmd/fast_delete/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"google.golang.org/api/iterator"
+	"os"
+	"sync"
+	"time"
+
+	"cloud.google.com/go/storage"
+)
+
+func main() {
+
+	flag.Parse()
+
+	if len(os.Args) != 2 {
+		//"dfuseio-global-blocks-us", "sol-mainnet/v1-oneblock"
+		fmt.Println("Require argument [bucket] [object-prefix]")
+		os.Exit(0)
+	}
+
+	bucket := os.Args[0]
+	objectPrefix := os.Args[1]
+
+	jobs := make(chan job, 1000)
+	var wg sync.WaitGroup
+
+	for w := 1; w <= 500; w++ {
+		wg.Add(1)
+		go worker(w, &wg, jobs)
+	}
+
+	_, err := listFiles(bucket, objectPrefix, func(bucket string, f string) {
+		jobs <- job{
+			bucket: bucket,
+			file:   f,
+		}
+	})
+	close(jobs)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Waiting ....")
+	wg.Wait()
+
+}
+
+type job struct {
+	bucket string
+	file   string
+}
+
+func worker(id int, wg *sync.WaitGroup, jobs <-chan job) {
+	defer wg.Done()
+	for j := range jobs {
+		fmt.Println("worker:", id, " deleting file: ", j.file)
+		err := deleteFile(j.bucket, j.file)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+// listFiles lists objects within specified bucket.
+func listFiles(bucket string, prefix string, f func(bucket string, file string)) ([]string, error) {
+	// bucket := "bucket-name"
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("storage.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*200)
+	defer cancel()
+
+	it := client.Bucket(bucket).Objects(ctx, &storage.Query{
+		Prefix: prefix,
+	})
+	var files []string
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("Bucket(%q).Objects: %v", bucket, err)
+		}
+		f(bucket, attrs.Name)
+		files = append(files, attrs.Name)
+	}
+	fmt.Println("ssss")
+	return files, nil
+}
+
+func deleteFile(bucket, object string) error {
+	// bucket := "bucket-name"
+	// object := "object-name"
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("storage.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+	defer cancel()
+
+	o := client.Bucket(bucket).Object(object)
+	if err := o.Delete(ctx); err != nil {
+		return fmt.Errorf("Object(%q).Delete: %v", object, err)
+	}
+	return nil
+}
+
+func createBucketClassLocation(projectID, bucketName string) error {
+	// projectID := "my-project-id"
+	// bucketName := "bucket-name"
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("storage.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+	defer cancel()
+
+	storageClassAndLocation := &storage.BucketAttrs{
+		StorageClass: "COLDLINE",
+		Location:     "asia",
+	}
+	bucket := client.Bucket(bucketName)
+	if err := bucket.Create(ctx, projectID, storageClassAndLocation); err != nil {
+		return fmt.Errorf("Bucket(%q).Create: %v", bucketName, err)
+	}
+	fmt.Printf("Created bucket %v in %v with storage class %v\n", bucketName, storageClassAndLocation.Location, storageClassAndLocation.StorageClass)
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,13 +3,18 @@ module github.com/dfuse-io/tooling
 go 1.14
 
 require (
-	github.com/abourget/viperbind v0.1.0
-	github.com/dfuse-io/logging v0.0.0-20201110202154-26697de88c79
-	github.com/dfuse-io/pbgo v0.0.5 // indirect
+	cloud.google.com/go/storage v1.0.0
 	github.com/eoscanada/eos-go v0.9.0
-	github.com/lithammer/dedent v1.1.0
+	github.com/golang/protobuf v1.3.4 // indirect
 	github.com/mr-tron/base58 v1.2.0
-	github.com/spf13/cobra v1.1.1
-	github.com/spf13/viper v1.7.1
+	go.opencensus.io v0.22.1 // indirect
 	go.uber.org/zap v1.14.0
+	golang.org/x/exp v0.0.0-20191030013958-a1ab85dbe136 // indirect
+	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
+	golang.org/x/sys v0.0.0-20190804053845-51ab0e2deafa // indirect
+	golang.org/x/tools v0.0.0-20191112195655-aa38f8e97acc // indirect
+	google.golang.org/api v0.13.0
+	google.golang.org/genproto v0.0.0-20191108220845-16a3f7862a1a // indirect
+	google.golang.org/grpc v1.26.0 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
