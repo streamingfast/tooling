39cb6b054c8c28288859634e7cbebbb7601a8050
Julien Cassis <juliencassis@gmail.com>
2022-03-23 11:06:24 -0400

    added decode type support on to_base58

--- cmd/to_base58/main.go
+++ cmd/to_base58/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/hex"
+	"flag"
 	"fmt"
 
 	"github.com/mr-tron/base58"
 	"github.com/streamingfast/tooling/cli"
 )
 
+var asHexFlag = flag.Bool("hex", false, "Decodes the input as a hex representation")
+var asBase64Flag = flag.Bool("b64", false, "Decodes the input as a standard base64 representation")
+
 func main() {
+	flag.Parse()
+
 	scanner := cli.NewArgumentScanner()
 	for element, ok := scanner.ScanArgument(); ok; element, ok = scanner.ScanArgument() {
 		fmt.Println(toBase58(element))
@@ -16,12 +23,21 @@ func main() {
 }
 
 func toBase58(element string) string {
-	if cli.HexRegexp.MatchString(element) {
-		bytes, err := hex.DecodeString(element)
-		cli.NoError(err, "invalid hex value %q", element)
-
-		return base58.Encode(bytes)
+	if element == "" {
+		return ""
 	}
 
-	return base58.Encode([]byte(element))
+	var bytes []byte
+	var err error
+	if *asHexFlag {
+		bytes, err = hex.DecodeString(element)
+		cli.NoError(err, "invalid hex value %q", element)
+	}
+
+	if *asBase64Flag {
+		bytes, err = base64.StdEncoding.DecodeString(element)
+		cli.NoError(err, "invalid base64 value %q", element)
+	}
+
+	return base58.Encode(bytes)
 }
